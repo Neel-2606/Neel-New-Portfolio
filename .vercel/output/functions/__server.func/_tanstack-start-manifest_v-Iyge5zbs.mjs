8d99c3ec163bfaeee525260e019b4902e75dbdd0
Neel-2606 <neelprajapati2601@gmail.com>
2026-06-14 00:27:31 +0530

    fix: remove .vercel/output from git so Vercel actually builds the project

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-Iyge5zbs.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/Neel New Portfolio/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-7f8FN7Bi.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-7f8FN7Bi.js" } }] }, "/": { filePath: "D:/Neel New Portfolio/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-Dty4uMix.js"] } } });
-export {
-  tsrStartManifest
-};
